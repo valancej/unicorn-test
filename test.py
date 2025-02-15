6909bd7f1bd3accff18033b6d3e86fa02892d838
Jeremy Valance <jeremyvalance@Jeremys-iMac.local>
2025-02-15 10:45:01 -0500

    add test

---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+import multiprocessing as mp
+
+cpu_count = mp.cpu_count()
+
+print(cpu_count)
+
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
+
+
